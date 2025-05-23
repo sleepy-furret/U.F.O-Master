63a83d6953afcc005c5c9a06cb91024fda71d8c4
Catgg54 <sleepy-furret@proton.me>
2025-05-23 16:47:33 -0300

    Init

---
+++ cow.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var sucked_up = false
+var init_pos
+
+func _ready() -> void:
+	init_pos = position
+
+func _physics_process(delta: float) -> void:
+	print(init_pos, position)
+	if position != init_pos:
+		pass
+	if sucked_up == true:
+		velocity.y = -120
+	if sucked_up == false and position != init_pos:
+		velocity.y = 120
+	if sucked_up == false and position == init_pos:
+		velocity.y = 0
+	move_and_slide()
---
+++ ufo.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+var speed = 250
+var accel = 30
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	
+	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
+	velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+	
+	move_and_slide()
+	
+
+
+func _on_ufo_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("suckupable"):
+		area.get_parent().sucked_up = true
+		
+		
+
+
+
+
+func _on_ufo_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("suckupable"):
+		area.get_parent().sucked_up = false
+
+
+func _on_ufo_area_inside_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_ufo_area_sucking_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("suckupable"):
+		area.get_parent().sucked_up = true
+		
+		
+
+
+func _on_ufo_area_sucking_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("suckupable"):
+		area.get_parent().sucked_up = false
+		
+		
